0176e6208371e475c7cd958291e423c340ce75a7
Janardhan Shingade <janardhanshingade@gmail.com>
2022-04-23 19:59:25 +0530

    phase5 practice Projects

---
+++ SpringBootApplication/src/main/java/com/example/demo/SpringBootAwsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SpringBootAwsApplicationTests {
+@Test
+void contextLoads() {
+}
+
+}
